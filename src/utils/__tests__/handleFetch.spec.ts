e253e2e9ab9fc0d4633d656ba74c6fc3f9c5111b
Skyler Avery <skyleravery42@gmail.com>
2024-02-01 16:27:53 +0000

    Add base structure for fetching characters with some tests

---
+++ src/utils/__tests__/handleFetch.spec.ts
@@ -0,0 +1,34 @@
+import { searchCharacters } from "../handleFetch";
+
+const unmockedFetch = global.fetch
+
+describe('handleFetch', () => {
+  afterEach(() => {
+    global.fetch = unmockedFetch
+  })
+
+  describe('Character query', () => {
+    it('will return a cached response should it exist and is still valid', () => {
+
+    });
+
+    it('will return all possible characters for a search query from the API if cache is invalid or missing', async () => {
+      const expectedResult = {
+        ok: true,
+        json: () => Promise.resolve([{ foo: 'bar' }])
+      }
+      global.fetch = jest.fn(() =>
+        Promise.resolve(expectedResult)
+      ) as jest.Mock
+      const result = await searchCharacters('Test', new AbortController().signal)
+      expect(result).toEqual([{
+        foo: 'bar'
+      }])
+    });
+
+    it('will return an empty array if an error with the API is encountered', async () => {
+      global.fetch = () => Promise.reject('418 I\'m a teapot')
+      await expect(searchCharacters('Test', new AbortController().signal)).rejects.toThrow('An error has occured with the API')
+    })
+  });
+})
---
+++ src/utils/handleFetch.ts
@@ -0,0 +1,17 @@
+const baseURL = 'https://rickandmortyapi.com/api';
+
+export async function searchCharacters(query: string, signal: AbortSignal) {
+  try {
+    const response = await fetch(`${baseURL}/character/?name=${query}`, {
+      signal,
+    })
+    if (!response.ok) {
+      throw new Error('An error has occured with the API')
+    }
+    const data = await response.json()
+    return data
+  } catch (e: any) {
+    console.error(e)
+    throw new Error('An error has occured with the API')
+  }
+}
