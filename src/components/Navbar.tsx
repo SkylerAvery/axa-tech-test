c2f7fc48a791a69a19c3f9fbdfa0111d37edb507
Skyler Avery <skyleravery42@gmail.com>
2024-02-02 10:23:56 +0000

    Add styling to navbar and results dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,60 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const NavList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  padding: 0.75rem 1.25rem;
+  background-color: rgb(22 78 99);
+  margin: 0;
+`;
+
+const NavLink = styled(Link)`
+  color: rgb(243 244 246);
+  font-weight: 700;
+  font-size: 1.25rem;
+  line-height: 1.75rem;
+  text-decoration: none;
+`
+
+const Heading = styled.h1`
+  font-weight: 800;
+  font-size: 1.75rem;
+  line-height: 2rem;
+  text-align: center;
+  padding: 0.75rem 0;
+`
+
+const Main = styled.main`
+  padding-bottom: 1.25rem;
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  display: flex;
+  justify-content: flex-start;
+  text-align: center;
+  flex-direction: column;
+`
 
 export default function Navbar () {
   return (
     <>
-      <p>Navbar</p>
-      <Outlet />
+      <nav>
+        <NavList>
+          <li>
+            <NavLink to="/">
+              Home
+            </NavLink>
+          </li>
+        </NavList>
+      </nav>
+      <Heading>
+        Rick and Morty Character Search
+      </Heading>
+      <Main>
+        <Outlet />
+      </Main>
     </>
   )
 }
--- src/components/ResultsDropdown.tsx
+++ src/components/ResultsDropdown.tsx
@@ -1,20 +1,61 @@
 import { useSelector } from "react-redux"
 import { GlobalState } from '../store'
 import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+const CharacterList = styled.ul`
+  list-style-type: none;
+  border-style: solid;
+  border-top: 0;
+  border-bottom-width: 2px;
+  border-left-width: 2px;
+  border-right-width: 2px;
+  border-bottom-left-radius: 1.5rem;
+  border-bottom-right-radius: 1.5rem;
+  border-color: rgb(229, 231, 235);
+  background-color: rgb(255, 255, 255);
+  overflow: scroll;
+  width: 75%;
+  height: 15rem;
+  padding: 0;
+  margin: 0 auto;
+`
+
+const CharacterListItem = styled.li`
+  border-color: rgb(209 213 219);
+  margin: 0.5rem 0rem;
+  width: 100%;
+  :hover {
+    background-color: rgb(125 211 252);
+  }
+  &:first-child {
+    margin: 0;
+  }
+`
+
+const CharacterListItemLink = styled(Link)`
+  font-weight: 700;
+  font-size: 1.25rem;
+  line-height: 1.75rem;
+  padding: 0.5rem 0rem;
+  text-decoration: none;
+  display: block;
+  color: rgb(55 65 81);
+`
 
 export default function ResultsDropdown() {
   const searchResults = useSelector((state: GlobalState) => state.character.searchResults)
   if (searchResults.length === 0) return null
 
   return (
-    <ul>
+    <CharacterList>
       {searchResults.map((character) => (
-        <li key={character.id}>
-          <Link to={`character/${character.id}`} state={character}>
+        <CharacterListItem key={character.id}>
+          <CharacterListItemLink to={`character/${character.id}`} state={character}>
             {character.name}
-          </Link>
-        </li>
+          </CharacterListItemLink>
+        </CharacterListItem>
       ))}
-    </ul>
+    </CharacterList>
   )
 }
